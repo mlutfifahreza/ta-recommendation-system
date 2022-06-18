d85b9bf8c1fcb63d417c2556e2d52d5bbd584404
mlutfifahreza <mlutfifahreza@gmail.com>
2022-06-18 14:58:34 +0700

    revamp log and variables

--- logic/data/extract_data-200.py
+++ logic/data/extract_data-200.py
@@ -1,13 +1,18 @@
-import os, json, csv, time, gc
+import os, json, csv, time
 from operator import itemgetter
 from tqdm import tqdm
 
-# Constants
+# General Variables
+READING_STRING = '\033[94m' + "Reading : " + '\033[0m'
+EXPORT_STRING = '\033[92m' + "Export : " + '\033[0m'
 COUNT_MPD_FILE = 1000 # number of files inside mpd data
 COUNT_MPD_FILE_DATA = 1000 # count of data inside each file
 COUNT_TO_SELECT = 200000
 COUNT_SELECTED = 0
 TIME_START = time.time()
+current_path = os.getcwd()
+mpd_path = current_path + "/data/mpd/spotify_million_playlist_dataset/data/"
+tracks = {}
 
 # Getting file names
 mpd_file_names = []
@@ -16,33 +21,27 @@ for i in range(COUNT_MPD_FILE):
     end = start+(COUNT_MPD_FILE_DATA-1)
     mpd_file_names.append("mpd.slice." + str(start) + "-" + str(end) + ".json")
 
-# Getting path
-current_path = os.getcwd()
-data_path = current_path + "/data/mpd/spotify_million_playlist_dataset/data/"
-playlists_csv_path = current_path + "/data/data-200/playlists.csv"
-tracks_csv_path = current_path + "/data/data-200/tracks.csv"
-popular_csv_path = current_path + "/data/data-200/popular.csv"
-tracks = {}
-
 # Generate CSV : Playlist
-with open(playlists_csv_path, 'w', encoding='UTF8', newline='') as f:
-    header = ["playlist_id","title","track_ids"]
-    print("Export : creating", playlists_csv_path)
+csv_name = "playlists.csv"
+with open(current_path + "/data/data-200/" + csv_name, 'w', encoding='UTF8', newline='') as f:
+    # starting
+    print(EXPORT_STRING, csv_name)
     writer = csv.writer(f)
+    # write header
+    header = ["playlist_id","title","track_ids"]
     writer.writerow(header)
-    
+    # read from mpd -> write content
     for i in range(COUNT_MPD_FILE):
-        opened_file = open(data_path+mpd_file_names[i])
+        opened_file = open(mpd_path+mpd_file_names[i])
         playlists_bacth_i = json.load(opened_file)["playlists"]
-
         # scanning each playlist
         for playlist in playlists_bacth_i:
             if (COUNT_SELECTED == COUNT_TO_SELECT) : break
-            # validate condition
+            # validate condition -> write
             if (playlist["num_tracks"] >= 51 and playlist["num_tracks"] <= 100 and playlist["num_tracks"] == len(playlist["tracks"])):
                 playlist_info = []
                 playlist_info.append(playlist["pid"])
-                playlist_info.append(playlist["name"])
+                playlist_info.append(playlist["name"].strip())
                 for track in playlist["tracks"]:
                     track_id = track["track_uri"].replace("spotify:track:","")
                     track_name = track["track_name"]
@@ -61,27 +60,32 @@ with open(playlists_csv_path, 'w', encoding='UTF8', newline='') as f:
                         }
                 writer.writerow(playlist_info)
                 COUNT_SELECTED += 1
-
             # progress stats
             time_elapsed = time.time()-TIME_START
             time_remaining = (COUNT_TO_SELECT-COUNT_SELECTED)/COUNT_SELECTED * time_elapsed
             progress_string = "Selected: " + str(COUNT_SELECTED) + "/" + str(COUNT_TO_SELECT)
             progress_string += " Elapsed: " + "{:.2f}".format(time_elapsed) + "s Remaining: " + "{:.2f}".format(time_remaining) + "s"
             print("\r"+progress_string, end ="")
-        
         # break condition
         if (COUNT_SELECTED == COUNT_TO_SELECT) : 
+            print("\n")
             break
-            print()
 
-# Generate CSV : tracks sorted popularity
-tracks_sorted = sorted(tracks.values(), key=itemgetter("count"), reverse=True) # returns list
-with open(tracks_csv_path, 'w', encoding='UTF8', newline='') as f:
-    header = ["track_id","track_name","artist_id","artist_name","count"]
-    print("Export : creating", tracks_csv_path)
+# Writing tracks.csv popularity sorted
+csv_name = "tracks.csv"
+with open(current_path + "/data/data-200/" + csv_name, 'w', encoding='UTF8', newline='') as f:
+    # starting
+    print(EXPORT_STRING, csv_name)
+    print("Please wait...", end="\r")
+    start_time = time.time()
     writer = csv.writer(f)
+    # write header
+    header = ["track_id","track_name","artist_id","artist_name","count"]
     writer.writerow(header)
-    for track in tqdm(tracks_sorted):
+    # write content
+    tracks_sorted = sorted(tracks.values(), key=itemgetter("count"), reverse=True) # returns list
+    total = len(tracks_sorted)
+    for track in tracks_sorted:
         writer.writerow([
             track["track_id"],
             track["track_name"],
@@ -89,3 +93,5 @@ with open(tracks_csv_path, 'w', encoding='UTF8', newline='') as f:
             track["artist_name"],
             track["count"]
         ])
+    time_elapsed = "{:.3f}".format(time.time()-start_time)
+    print(f"Done in {time_elapsed}s\n")
--- logic/data/extract_playlist_titles.py
+++ logic/data/extract_playlist_titles.py
@@ -1,37 +1,43 @@
 import os, csv, time
 
-# Getting path
+# General Variables
+READING_STRING = '\033[94m' + "Reading : " + '\033[0m'
+EXPORT_STRING = '\033[92m' + "Export : " + '\033[0m'
 current_path = os.getcwd()
-playlists_csv_path = current_path + "/data/data-200/playlists.csv"
-playlist_titles_csv_path = current_path + "/data/data-200/playlist_titles.csv"
-titles = []
+titles = {}
 
 # Reading playlists.csv dataset
-titles = {}
-with open(playlists_csv_path) as csv_file:
-    print("Reading :",playlists_csv_path)
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    is_at_header = True
+csv_name = "playlists.csv"
+with open(current_path + "/data/data-200/" + csv_name) as csv_file:
+    # starting
+    print(READING_STRING, csv_name)
     print("Please wait...", end="\r")
     start_time = time.time()
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    is_at_header = True
     for row in csv_reader:
         if is_at_header:
             is_at_header = False
         else:
-            titles[row[1].strip()] = None
+            titles[row[1]] = None
     time_elapsed = "{:.3f}".format(time.time()-start_time)
     print(f"Done in {time_elapsed}s\n")
 
 # Writing to titles.csv
-header = ["title"]
-with open(playlist_titles_csv_path, 'w', encoding='UTF8', newline='') as f:
-    print("Creating :", playlist_titles_csv_path)
+csv_name = "titles.csv"
+with open(current_path + "/data/data-200/" + csv_name, 'w', encoding='UTF8', newline='') as f:
+    # starting
+    print(EXPORT_STRING, csv_name)
+    print("Please wait...", end="\r")
+    start_time = time.time()
     writer = csv.writer(f)
+    # write header
+    header = ["title"]
     writer.writerow(header)
+    # write content
     sorted_titles = sorted(titles.keys())
     total = len(titles)
     print("Please wait...", end="\r")
-    start_time = time.time()
     for title in sorted_titles:
         writer.writerow([title])
     time_elapsed = "{:.3f}".format(time.time()-start_time)
