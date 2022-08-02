10181ec187bc8e6e76ebad8fff5ecf927c7f0d70
mlutfifahreza <mlutfifahreza@gmail.com>
2022-08-02 18:03:55 +0700

    refactor fcm

--- logic/1-model/p7_model_train_fcm.py
+++ logic/1-model/p7_model_train_fcm.py
@@ -8,26 +8,18 @@ EXPORT_STRING = '\033[92m' + "Export :" + '\033[0m'
 PROCESS_STRING = '\033[35m' + "Process :" + '\033[0m'
 root_path = os.getcwd()
 
-# FCM: variables default
-n_clusters = 4
-n_data = 120
-n_dimension = 2
-fuzzy_param = 1.8
-max_iter = 20
-threshold = 1e-6
-
 # FCM: helper functions
 def square_dist(x, y):
     return np.sum(np.square(x - y))
 
-def SSE(weights, inputs, centroids, p = fuzzy_param):
+def SSE(weights, inputs, centroids, p = 1.75):
     sse = 0
     for k in range(n_clusters):
         for i in range(n_data):
             sse += (weights[i,k] ** p) * square_dist(inputs[i], centroids[k])
     return sse
 
-def get_centroids(weights, inputs, p = fuzzy_param):
+def get_centroids(weights, inputs, p = 1.75):
     centroids = np.zeros((n_clusters, n_dimension))
     for k in range(n_clusters):
         denom = 0
@@ -39,7 +31,7 @@ def get_centroids(weights, inputs, p = fuzzy_param):
             centroids[k,d] = num/denom
     return centroids
 
-def update_weights(weights, inputs, centroids, p = fuzzy_param):
+def update_weights(weights, inputs, centroids, p = 1.75):
     new_weights = np.copy(weights)
     for k in range(n_clusters):
         for i in range(n_data):
@@ -82,31 +74,39 @@ n_clusters = n_data//200
 n_dimension = inputs.shape[1]
 fuzzy_param = 1.8
 max_iter = 20
-threshold = 1e-6
+threshold = 1e-10
 
 # FCM: init weights, centroids
 weights = np.random.rand(n_data, n_clusters)
 centroids = None
 # FCM: learn
 print(PROCESS_STRING, "FCM Learning")
+print(f"Parameters:"
+    + f"\n  fuzzy param = {fuzzy_param} "
+    + f"\n  threshold = {threshold} "
+    + f"\n  num of clusters = {n_clusters}")
 log_sse = []
+log_sse_diff = []
 last_sse = float("inf")
 for i in range(max_iter):
     # For progress checking
     t_start = time.time()
     print(f"Iteration: {i+1}/{max_iter} Status: computing...", end ="\r")
     # learn
-    new_centroids = get_centroids(weights, inputs)
-    new_weights = update_weights(weights, inputs, new_centroids)
-    new_sse = SSE(new_weights, inputs, new_centroids)
-    if (last_sse - new_sse) > threshold:
+    new_centroids = get_centroids(weights, inputs, fuzzy_param)
+    new_weights = update_weights(weights, inputs, new_centroids, fuzzy_param)
+    new_sse = SSE(new_weights, inputs, new_centroids, fuzzy_param)
+    sse_diff = last_sse - new_sse
+    if (sse_diff) > threshold:
         weights = new_weights
         centroids = new_centroids
         last_sse = new_sse
         log_sse.append(new_sse)
+        log_sse_diff.append(sse_diff)
         # progress stats
         t_elapsed = time.time()-t_start
-        print(f"\rIteration: {i+1}/{max_iter} Status: Done - Elapsed: {t_elapsed:.3f}s Error: {new_sse}")
+        print(f"\rIteration: {i+1}/{max_iter} Status: Done - "
+            + f"Elapsed: {t_elapsed:.3f}s E: {new_sse} dE:{sse_diff}")
     else:
         print(f"\rIteration: {i+1}/{max_iter} Status: stopping, change of error < threshold")
         break
